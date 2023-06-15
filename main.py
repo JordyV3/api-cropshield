257e49649faa334c0011a6fb9fa9d8047f499875
Jordy Vega <81053948+JordyV3@users.noreply.github.com>
2023-06-14 21:03:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ async def predict(
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
     confidence = np.max(predictions[0])
     return {
-        'class': predicted_class,
-        'confidence': float(confidence)
+        'Predicción': predicted_class,
+        'Confianza': float(confidence)
     }
 
 
